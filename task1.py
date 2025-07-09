31624d9d6972e5959489c93d8fa5f74874a445d4
Anshul Kavishwar <135499399+Anshulkavi@users.noreply.github.com>
2025-07-09 18:51:16 +0530

    added task1 and task2

---
+++ task1.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("Addition: ",a + b)
+print("Substraction: ",a - b)
+print("Mutliplication: ",a * b)
+
+if b != 0:
+    print("Division:", a / b)
+else:
+    print("Division: Cannot divide by zero")
